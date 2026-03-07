334fcace3745c8a8cf0014396fdd09f60f0fc4e1
dtanglao-tech <dtanglao@byupathway.edu>
2026-03-07 19:14:30 +0800

    Number List

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -36,7 +36,7 @@ class Program
         Console.WriteLine($"The sum is: {sum}");
 
         float average = (float)sum / numbers.Count;
-        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The average is: {average:F2}");
 
         int max = numbers[0];
         foreach (int number in numbers)
