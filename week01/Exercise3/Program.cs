79188b5c718b4c1bdb23bf80b67446b2018ffdfe
dtanglao-tech <dtanglao@byupathway.edu>
2026-03-07 13:58:23 +0800

    Guess game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            int number = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != number)
+            {
+                Console.Write("Guess a number between 1 and 100: ");
+                string answer = Console.ReadLine();
+                
+                try
+                {
+                    guess = int.Parse(answer);
+                    guessCount++;
+                    
+                    if (guess < number)
+                    {
+                        Console.WriteLine("Too low!");
+                    }
+                    else if (guess > number)
+                    {
+                        Console.WriteLine("Too high!");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    guess = -1;
+                }
+            }
+            Console.WriteLine("Congratulations! You guessed the number.");
+            Console.WriteLine($"It took you {guessCount} guesses!");
+            
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            response = Console.ReadLine().ToLower();
+        }
     }
 }
