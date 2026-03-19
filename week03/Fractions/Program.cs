ac44eb1cc4fa9a967f4da2f612f18d21793c9c99
dtanglao-tech <dtanglao@byupathway.edu>
2026-03-19 17:17:45 +0800

    Fraction code

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        // Testing for getters and setters
+        Console.WriteLine("\n Testing getters and setters:");
+
+        Fraction test = new Fraction();
+        
+        test.SetTop(6);
+        test.SetBottom(7);
+
+        Console.WriteLine(test.GetFractionString());
+        Console.WriteLine(test.GetTop());
+        Console.WriteLine(test.GetBottom());
     }
 }
